e41f2ef2fe6f5728f5c227320e6ef348c3dfca0e
sachidanand dubey <sachidanand@subcodevs.com>
2023-11-30 10:40:24 +0530

    datatypeConversion

---
+++ datatypeConversion.js
@@ -0,0 +1,24 @@
+let score=53
+console.log(typeof score);
+console.log(typeof (score));
+let marks="55"
+changeType=Number(marks)
+console.log(changeType);
+
+//Note
+//"55"=>55
+//"55abc"=>NaN
+//boolean=>1,0
+
+let boolenType=1
+let changeBoolean=Boolean(boolenType)
+console.log(changeBoolean);
+
+//1=>true 0=>false
+//"abc"=>true
+//""=>false
+
+let strNumber=44
+let stringNumber=String(strNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
--- variable.js
+++ variable.js
@@ -2,6 +2,7 @@ const accountId=1234 //const varibale not change
 let accountName='sachin' 
 var accountType='Debit'
 accountEmail="abc@gmail.com"
-//do not use var keyword block scope issue and functional issue
+//do not use var keyword block scope issue and functional scope issue
+//prefer not to use var because of issue in block scope and functional scope
 
 console.table([accountId,accountName,accountType,accountEmail]);
