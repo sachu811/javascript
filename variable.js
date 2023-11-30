af74474e4a01d52b46dcba820b560ed4118624aa
sachidanand dubey <sachidanand@subcodevs.com>
2023-11-30 09:49:41 +0530

    variable in javascript

---
+++ variable.js
@@ -0,0 +1,7 @@
+const accountId=1234 //const varibale not change
+let accountName='sachin' 
+var accountType='Debit'
+accountEmail="abc@gmail.com"
+//do not use var keyword block scope issue and functional issue
+
+console.table([accountId,accountName,accountType,accountEmail]);
